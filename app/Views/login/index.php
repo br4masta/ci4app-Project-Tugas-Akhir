04efea36d61cff6fcff1395550eea7cb8ffb2418
faisalvice <faisalvice@gmail.com>
2021-06-18 21:04:51 +0700

    front login

---
+++ app/Views/login/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <main class="form-signin">
+                <img class="img"class="mb-4" src="logo-1.png" alt="" width="62" height="67">
+                <h1>LOGIN</h1>
+                <form action="">
+                    <input type="text" placeholder="Username">
+                    <input type="text" placeholder="Password">
+                    <input type="submit" value="LOGIN">
+                </form>
+            </main>
+        </div>
+    </body>
+</html>
