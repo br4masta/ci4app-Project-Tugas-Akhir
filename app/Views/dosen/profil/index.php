36474caff4960b63590cf53ece56a6b4ca03d1d4
Akanerd <muhammad.hafizh2016@gmail.com>
2021-07-07 19:35:50 +0700

    update profil dosen

--- app/Views/dosen/profil/index.php
+++ app/Views/dosen/profil/index.php
@@ -1,12 +1,12 @@
 <?= $this->extend('templates/index'); ?>
 
 <?= $this->section('isi'); ?>
-<!-- Content Header (Page header) -->
+
 <section class="content-header">
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h3>Profil Dosen</h3>
+                <h1>Profile</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
@@ -21,26 +21,22 @@
 <!-- Main content -->
 <section class="content">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3">
-                <div class="viewdatadsn">
-                    
-                </div>
+        <div class="row viewdatamhs">
+
         </div><!-- /.container-fluid -->
     </div>
-</div>
 
 </section>
 <!-- viewModal - Section -->
 
 <!-- Page specific script -->
 <script>
-    function data_dsn() {
+    function datamahasiswa_profil() {
         $.ajax({
-            url: "<?= site_url('dosen/index/id') ?>",
+            url: "<?= site_url('dosen/showprofil/id') ?>",
             dataType: "json",
             success: function(response) {
-                $('.viewdatadsn').html(response.data);
+                $('.viewdatamhs').html(response.data);
             },
             error: function(xhr, ajaxOptions, thrownError) {
                 alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
@@ -49,10 +45,9 @@
     }
 
     $(document).ready(function() {
-        data_dsn();
+        datamahasiswa_profil();
     });
 </script>
-
 <?= $this->endSection(); ?>
 
 <?= $this->include('dosen/menu'); ?>
